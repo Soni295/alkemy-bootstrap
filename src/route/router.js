8542931d84e49363de114506ae170224fe1e6cac
Sion Ramos de los Santos <sionandrieux@hotmail.com>
2021-07-23 19:34:14 -0300

    Cree la pagina de compra
      * en la cual se mostrara cuando se ha vendio un
      * producto

--- src/route/router.js
+++ src/route/router.js
@@ -2,8 +2,13 @@ const router = require('express').Router()
 const { join } = require('path')
 const STATIC = require('../PATH')
 
-router.get('/', (req, res) => {
+const home = (req, res) => (
   res.sendFile(join(STATIC, 'index.html'))
-})
+)
+const compra = (req, res) => (
+  res.sendFile(join(STATIC, 'compra.html'))
+)
 
 module.exports = router
+  .get('/', home)
+  .get('/compra.html', compra)
