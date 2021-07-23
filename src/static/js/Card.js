6ebff0b721168ff600fbf7b87bc02d6aedd094cb
Sion Ramos de los Santos <sionandrieux@hotmail.com>
2021-07-23 13:59:44 -0300

    add modal for buy

--- src/static/js/Card.js
+++ src/static/js/Card.js
@@ -6,17 +6,26 @@ const handleProduct = (title, price) => {
   console.log(title, price)
 }
 
-export const Card = ({title='example', img=exampleImg, price='$100'}) => {
-  const titleCard = create('h3', {className: 'card-title mx-3', innerHTML:title })
-  const priceCard = create('p', {className: 'card-text h5 my-3 mx-3', innerHTML: price})
-  const imgCard = create('img', {className: 'card-img-top', src: img})
 
+const BtnBuy = (title, price) => {
   const btnBuy = create('input', {
     className: 'btn btn-primary btn-lg my-2 mx-3',
     type: 'button',
     value: 'Comprar',
     onclick: () => handleProduct(title, price)
   })
+
+  btnBuy.setAttribute("data-bs-toggle", 'modal');
+  btnBuy.setAttribute("data-bs-target", '#exampleModal');
+  return btnBuy
+}
+
+export const Card = ({title='example', img=exampleImg, price='$100'}) => {
+  const titleCard = create('h3', {className: 'card-title mx-3', innerHTML:title })
+  const priceCard = create('p', {className: 'card-text h5 my-3 mx-3', innerHTML: price})
+  const imgCard = create('img', {className: 'card-img-top', src: img})
+
+  const btnBuy = BtnBuy(title, price)
 
   const cardBody = create('div', {className: 'card-body'}, [titleCard, priceCard, btnBuy])
   const card = create('div', {className: 'card my-3'}, [imgCard, cardBody])
