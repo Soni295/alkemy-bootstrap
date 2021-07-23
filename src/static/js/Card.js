485d0db42a87e0657240a27b473c1c4574b4b1b2
Sion Ramos de los Santos <sionandrieux@hotmail.com>
2021-07-23 12:28:56 -0300

    Mejore el estilo
      * agrege la imagen de la categoria
      * ordene un poco los estilos de las Cards

--- src/static/js/Card.js
+++ src/static/js/Card.js
@@ -6,24 +6,20 @@ const handleProduct = (title, price) => {
   console.log(title, price)
 }
 
-const descExample = 'example Desc'
-
-export const Card = ({title='example', img=exampleImg, price='$100', desc=descExample}) => {
-  const titleCard = create('h3', {className: 'card-title', innerHTML:title })
-  const descCard = create('p', {className: 'card-text px-2 h6', innerHTML: desc})
-  const priceCard = create('p', {className: 'card-text h5', innerHTML: price})
+export const Card = ({title='example', img=exampleImg, price='$100'}) => {
+  const titleCard = create('h3', {className: 'card-title mx-3', innerHTML:title })
+  const priceCard = create('p', {className: 'card-text h5 my-3 mx-3', innerHTML: price})
   const imgCard = create('img', {className: 'card-img-top', src: img})
 
   const btnBuy = create('input', {
-    className: 'btn btn-primary',
+    className: 'btn btn-primary btn-lg my-2 mx-3',
     type: 'button',
     value: 'Comprar',
     onclick: () => handleProduct(title, price)
   })
 
-  const cardBody = create('div', {className: 'card-body'}, [titleCard, descCard, priceCard, btnBuy])
+  const cardBody = create('div', {className: 'card-body'}, [titleCard, priceCard, btnBuy])
   const card = create('div', {className: 'card my-3'}, [imgCard, cardBody])
   const col = create('div', {className: 'col-lg-3 col-sm-6'}, [card])
-
   return col
 }
